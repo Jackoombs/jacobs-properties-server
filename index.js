f7e23c58477232cb641538c8ae3c53d80fe6a2ec
Jackoombs <jack.coombs@hotmail.co.uk>
2022-09-21 17:11:03 +0100

    Updated server to fetch letting and sales. Added enpoints for search type.

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ import { fetchAllProperties, fetchProperty } from "./reapitFetch.js";
 import _ from "lodash"
 
 const app = express();
-const port = 8080;
-const properties = []
+const port = process.ENV.PORT || 8080;
+let salesProperties = []
+let lettingsProperties = []
 
 app.listen(port, () => {
   console.log(`server listening at http://localhost:${port}`);
 });
 
-const getProperties = async () => {
-  const propertyIDs = await fetchAllProperties()
+const getProperties = async (emptyArray, SearchType) => {
+  const propertyIDs = await fetchAllProperties(SearchType)
   for (const ID of propertyIDs) {
     const propertyData = await fetchProperty(ID)
     const propertySubset = _.pick(propertyData, 
@@ -23,30 +24,46 @@ const getProperties = async () => {
         "Status",
         "PriceString", 
         "TotalBedrooms",
-        "TotalBathrooms",
+        "Bathrooms",
 
       ])
-    console.log(propertyData)
-    properties.push(propertySubset)
+    emptyArray.push(propertyData)
   }
+  return emptyArray
 }
 
-await getProperties()
-console.log(properties[0].SalePrice)
+await getProperties(salesProperties, "sales")
+await getProperties(lettingsProperties, "lettings")
+console.log(lettingsProperties)
+
 setInterval(async () => {
-  console.log(await getProperties())
-},[100000])
+  const newSalesProperties = await getProperties([], "sales")
+  const newLettingsProperties = await getProperties([], "lettings")
+  salesProperties = newSalesProperties
+  lettingsProperties = newLettingsProperties
+},[800000])
 
 app.get("/", (req, res) => {
   res.json({msg: "API working"})
 })
 
-app.get("/properties", (req, res) => {
+app.get("/properties/:searchType", (req, res) => {
+  const searchType = req.params.searchType
+  let properties = []
+  searchType === "sales" 
+    ? properties = salesProperties
+    : properties = lettingsProperties
   res.json(properties)
+})
+
+app.get("/properties", (req, res) => {
+  const allProperties = salesProperties.concat(lettingsProperties)
+  res.json(allProperties)
 })
 
 app.get("/property/:id", (req, res) => {
   const ID = req.params.id
-  const property = properties.find(property => property.ID === ID)
+  const allProperties = salesProperties.concat(lettingsProperties)
+  const property = allProperties.find(property => property.ID === ID)
   res.json(property)
 })
