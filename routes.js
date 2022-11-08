8f76e6b7d3c1b2da4f58ba72ec69a0d7657a007e
Jackoombs <jack.coombs@hotmail.co.uk>
2022-11-08 22:02:03 +0000

    Exapnded the send route to handle errors.

--- routes.js
+++ routes.js
@@ -1,38 +1,48 @@
 import { getSales, getLettings } from "./index.js";
-import { sendMail } from "./mail.js"
+import { sendUserMail } from "./mail.js";
+import { generateCode } from "./utils.js";
 import express from "express";
-const router = express.Router()
+const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({msg: "API working"})
-})
+  res.json({ msg: "API working" });
+});
 
 router.get("/properties/:searchType", (req, res) => {
-  const searchType = req.params.searchType
-  let properties = []
-  searchType === "sales" 
-    ? properties = getSales()
-    : properties = getLettings()
-  res.json(properties)
-})
+  const searchType = req.params.searchType;
+  let properties = [];
+  searchType === "sales"
+    ? (properties = getSales())
+    : (properties = getLettings());
+  res.json(properties);
+});
 
 router.get("/properties", (req, res) => {
-  let allProperties = getSales()
+  let allProperties = getSales();
   if (getLettings().length) {
-    allProperties = getSales().concat(getLettings())
+    allProperties = getSales().concat(getLettings());
   }
-  res.json(allProperties)
-})
+  res.json(allProperties);
+});
 
 router.get("/property/:id", (req, res) => {
-  const ID = req.params.id
-  const allProperties = getSales().concat(getLettings)
-  const property = allProperties.find((property) => property.ID === ID)
-  res.json(property)
-})
+  const ID = req.params.id;
+  const allProperties = getSales().concat(getLettings);
+  const property = allProperties.find((property) => property.ID === ID);
+  res.json(property);
+});
 
-router.post("/send", (req, res) => {
-  const { firstName, secondName, email } = req.body
-  sendMail(firstName, secondName, email)
-})
-export default router
+router.post("/send", async (req, res) => {
+  const code = generateCode(8)
+  const user = req.body.user
+  const friend = req.body.friend
+  try {
+    await sendUserMail(user.name, user.email, code);
+    await sendUserMail(friend.name, friend.email, code)
+    res.status(200)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ error: error })
+  }
+});
+export default router;
