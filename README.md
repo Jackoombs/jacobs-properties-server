1457fc2729071f1ba7a9a8d5c2f530d8a67bdc94
Jackoombs <jack.coombs@hotmail.co.uk>
2022-11-08 22:01:06 +0000

    Formatting changes.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jacobs-properties-server
+
 A caching server that feeds in the Reapit API and holds it in memory to serve the client.
--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 import express from "express";
 import { getProperties } from "./utils.js";
-import _ from 'lodash'
+import _ from "lodash";
 import router from "./routes.js";
-import dotenv from 'dotenv';
-import cors from "cors"
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static('public')); 
-app.use('/images', express.static('images'));
-app.use(cors({
-  origin: ["https://jacobs-properties-client.vercel.app", "https://staging.jacobs.properties"]
-}))
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
+app.use(
+  cors({
+    origin: [
+      "https://jacobs-properties-client.vercel.app",
+      "https://staging.jacobs.properties",
+      "https://jacobs.properties",
+      "http://localhost:3000"
+    ],
+  })
+);
 app.use(express.json());
 
-let salesProperties = []
-let lettingsProperties = []
-export const getSales = () => salesProperties
-export const getLettings = () => lettingsProperties
+let salesProperties = [];
+let lettingsProperties = [];
+export const getSales = () => salesProperties;
+export const getLettings = () => lettingsProperties;
 
 app.listen(port, () => {
   console.log(`server listening at http://localhost:${port}`);
 });
-app.use("/", router)
+app.use("/", router);
 
-await getProperties(lettingsProperties, "lettings")
-await getProperties(salesProperties, "sales")
+await getProperties(lettingsProperties, "lettings");
+await getProperties(salesProperties, "sales");
 
 setInterval(async () => {
-  const newSalesProperties = await getProperties([], "sales")
-  const newLettingsProperties = await getProperties([], "lettings")
-  salesProperties = newSalesProperties
-  lettingsProperties = newLettingsProperties
-}, 800000)
-
-
+  const newSalesProperties = await getProperties([], "sales");
+  const newLettingsProperties = await getProperties([], "lettings");
+  salesProperties = newSalesProperties;
+  lettingsProperties = newLettingsProperties;
+}, 800000);
